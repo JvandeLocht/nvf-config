e98eccf42aa126359df93d30e5782af9f751b2dd
Jan van de Locht <jan@vandelocht.uk>
2025-01-11 21:17:11 +0100

    Added highlight on yank

--- config/init.lua
+++ config/init.lua
@@ -1,2 +1,20 @@
-vim.opt.backup = false      -- Disable backup files
-vim.opt.writebackup = false -- Disable writing backup files
+VIM = vim
+VIM.opt.backup = false      -- Disable backup files
+VIM.opt.writebackup = false -- Disable writing backup files
+
+
+-- Create a group for the highlight autocommand (optional, but good practice)
+local highlight_group = VIM.api.nvim_create_augroup("HighlightYank", { clear = true })
+
+-- Highlight yanked text briefly using the IncSearch group
+VIM.api.nvim_create_autocmd("TextYankPost", {
+        group = highlight_group,
+        pattern = "*",
+        callback = function()
+                VIM.highlight.on_yank({
+                        higroup = "IncSearch",
+                        timeout = 150,
+                        on_visual = true, -- set to false to skip visual selections
+                })
+        end,
+})
